a8a0127c4c7338a958ef6aca104a4348b5cd08c5
Samson-02 <88614262+Samson-02@users.noreply.github.com>
2021-08-26 00:20:07 +0530

    (demo)
    Aim  ;- 
    1.  Initialing 2 (3*3) arrays with default values given.
    2.  Perform addition of elements in the matrices using a loop 
    3.  Displaying the sum of the matrices.

---
+++ Addition of  2 matrices.java
@@ -0,0 +1,15 @@
+package com.company;
+public class Main
+{
+	public static void main(String[] args) {
+		int X[][] = {{1,3,4},{2,4,3},{3,4,5}};
+		int Y[][] = {{1,3,4},{2,4,3},{1,2,4}};
+		System.out.println("The sum of the matrices is :- ");
+		for(int i=0; i<3; i++) {
+		for(int j=0; j<3; j++) {
+		    System.out.print((X[i][j] + Y[i][j]) + "  ");
+		}
+	     System.out.println();
+		}
+	}
+}
